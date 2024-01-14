7f399e22170a440d0996be24b777c84788a9b0f7
agis09 <agis09m@gmail.com>
2024-01-14 17:14:33 +0900

    feat: firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { initializeFirebaseApp } from '@src/lib/firebase/firebase'
+import { getApp } from 'firebase/app'
 
+initializeFirebaseApp()
 function Chat({ Component, pageProps }: AppProps) {
+  console.log(getApp())
   return (
     <ChakraProvider>
       <Component {...pageProps} />

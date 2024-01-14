91890e2351d584170082a104b3436c405a4ea4ad
agis09 <agis09m@gmail.com>
2024-01-14 16:00:52 +0900

    feat: chakuraui

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { Box, Heading } from '@chakra-ui/react'
+
+const Page: NextPage = () => {
+  return <Box>
+    <Heading>Chakura UI</Heading></Box>
+}
+export default Page

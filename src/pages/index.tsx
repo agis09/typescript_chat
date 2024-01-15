5b8d1d4c3000e1164e7bfb4e71642f878716df77
agis09 <agis09m@gmail.com>
2024-01-15 14:10:50 +0900

    feat: authgrad

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
-import { Box, Heading } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
+import { AuthGuard } from '@src/feature/auth/component/AuthGuard/AuthGuard'
+
 
 const Page: NextPage = () => {
-  return <Box>
-    <Heading>Chakura UI</Heading></Box>
+  return (
+    <AuthGuard>
+      <Heading>Chakura UI</Heading>
+    </AuthGuard>
+  )
 }
 export default Page

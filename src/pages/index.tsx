df32accf8feef285108060908848dd82ffa76a57
agis09 <agis09m@gmail.com>
2024-01-17 19:55:17 +0900

    feat: header & pathpida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from 'next'
 import { Heading } from '@chakra-ui/react'
-import { AuthGuard } from '@src/feature/auth/component/AuthGuard/AuthGuard'
 
 
 const Page: NextPage = () => {
   return (
-    <AuthGuard>
-      <Heading>Chakura UI</Heading>
-    </AuthGuard>
+    <Heading>Chakura UI</Heading>
   )
 }
 export default Page
